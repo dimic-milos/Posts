33339397b81271a37c08419fdd2a15f00745c50e
Milos Dimic <milos.dimic@sevdesk.de>
2023-09-07 14:42:18 +0200

    Added AppCoordinator

---
+++ Posts/AppCoordinator/AppCoordinatorView.swift
@@ -0,0 +1,18 @@
+//
+//  AppCoordinatorView.swift
+//  Posts
+//
+//  Created by Milos Dimic on 07.09.23.
+//
+
+import SwiftUI
+import FlowStacks
+
+struct AppCoordinatorView: View {
+
+    @State private var viewModel: AppCoordinatorViewModel
+
+    var body: some View {
+        Text("1")
+    }
+}
---
+++ Posts/AppCoordinator/AppCoordinatorViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinatorViewModel.swift
+//  Posts
+//
+//  Created by Milos Dimic on 07.09.23.
+//
+
+import Foundation
+import FlowStacks
+
+@Observable final class AppCoordinatorViewModel {
+
+}
+
+// MARK: - Screen
+
+extension AppCoordinatorViewModel {
+
+    enum Screen {
+
+        case login
+        case posts
+    }
+}
